1c8eaaf79afcea49540558d5c359c7a07262415d
ammar <bodla.pu@gmail.com>
2023-01-27 20:01:34 +0800

    changes done

--- assets/js/app.js
+++ assets/js/app.js
@@ -291,7 +291,7 @@ var pmu_ppu = L.geoJson(null, {
       }
     });
     var greenIcon = new LeafIcon({
-      iconUrl: 'http://121.121.232.54/sesb/assets/img/pmu.png'
+      iconUrl: 'http://121.121.232.54:88/sesb/assets/img/pmu.png'
     })
     return L.marker(latlng, {icon: greenIcon});
   },
